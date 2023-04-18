488f99dd59a8bad13a32f4c81374f5870317a73f
Junsung Choi <choiexe1@gmail.com>
2023-04-18 10:05:44 +0900

    add :: when application initialize, seed to redis

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ProductService } from './product/product.service';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  async onModuleInit() {
+    await this.productService.initProductsCache();
+    //await this.productService.seedProduct();
+  }
+
+  constructor(private readonly productService: ProductService) {}
+
   getHello(): string {
     return 'asd';
   }
